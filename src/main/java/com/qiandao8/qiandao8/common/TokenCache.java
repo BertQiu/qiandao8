8a0ee9f8a2b23d05666caa1e5229f1b013f1bb38
BertQ <bert_Q@aliyun.com>
2019-10-17 17:34:11 +0800

    edit by Bert Q ----第16次更新---- 将TokenCache配置从代码抽离出来，以后可以在application.properties中配置这些常量

--- src/main/java/com/qiandao8/qiandao8/common/TokenCache.java
+++ src/main/java/com/qiandao8/qiandao8/common/TokenCache.java
@@ -5,7 +5,11 @@ import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -13,8 +17,8 @@ import java.util.concurrent.TimeUnit;
  * ClassName : TokenCache
  * Description 存放Token的缓存
  */
+@Component
 public class TokenCache {
-
     /**
      * 缓存初始化容量
      */
@@ -29,6 +33,22 @@ public class TokenCache {
      * 缓存失效时间 单位（秒）
      */
     private static int EXPIRE_DURATION =5;
+
+    /**
+     * 使用静态代码块加载配置文件，覆盖原先的默认值
+     */
+    static {
+        InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("application.properties");
+        try {
+            Properties properties = new Properties();
+            properties.load(is);
+            INITIAL_CAPACITY = Integer.parseInt(properties.get("TokenCache.INITIAL_CAPACITY").toString());
+            MAXIMUM_SIZE = Integer.parseInt(properties.get("TokenCache.MAXIMUM_SIZE").toString());
+            EXPIRE_DURATION = Integer.parseInt(properties.get("TokenCache.EXPIRE_DURATION").toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     private static Logger logger = LoggerFactory.getLogger(TokenCache.class);
 
